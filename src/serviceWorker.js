b0a9181b488c9af9ad75996f5c66246b888e1a0b
Sebastian <sebgertz@gmail.com>
2019-01-02 20:49:54 -0500

    feat: register serviceWorker for offline use in production

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,12 +1,12 @@
 // This optional code is used to register a service worker.
 // register() is not called by default.
-
+//
 // This lets the app load faster on subsequent visits in production, and gives
 // it offline capabilities. However, it also means that developers (and users)
 // will only see deployed updates on subsequent visits to a page, after all the
 // existing tabs open on the page have been closed, since previously cached
 // resources are updated in the background.
-
+//
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read http://bit.ly/CRA-PWA
 
@@ -20,7 +20,7 @@ const isLocalhost = Boolean(
     )
 );
 
-export function register(config) {
+export default function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
