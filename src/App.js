62e06ed94b52dba4cc288737176706dad81a8277
Sebastian <sebgertz@gmail.com>
2019-01-03 16:54:02 -0500

    feat: include modal to display error message in case of failed fetch request

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import MapContainer from './Components/MapContainer';
 import 'whatwg-fetch';
 import './css/App.css';
+import ErrorModal from './Components/ErrorModal.js'
 
 class App extends Component {
 
@@ -9,10 +10,19 @@ class App extends Component {
     super();
 
     this.state = {
-      museums: []
+      museums: [],
+      modal: false,
     };
   }
 
+  // pull up error modal if initial API call fails
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+  // fetch museums data as soon as component mounts and store in state
   componentDidMount() {
     const url = 'https://api.foursquare.com/v2/venues/explore?';
     const CLIENT_ID = process.env.REACT_APP_FOURSQUARE_ID;
@@ -27,19 +37,19 @@ class App extends Component {
     }
     fetch(url + new URLSearchParams(params), {
       method: 'GET'
-    }).then(response => response.json()).then(response => this.setState({ museums: response.response.groups[0].items}))
+    }).then(response => response.json()).then(response => this.setState({ museums: response.response.groups[0].items})).catch(() => this.toggle())
   }
 
   render() {
-
     return (
       <div className="App">
         <header className="App-header">
-          <p>
+          <p tabIndex="1">
             NYC Museums Explorer
           </p>
         </header>
-        <MapContainer museums={this.state.museums}/>
+        <MapContainer museums={this.state.museums}  />
+        <ErrorModal toggle={this.toggle} modal={this.state.modal}/>
       </div>
     );
   }
