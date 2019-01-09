4084582bdfe28d2748a228ce68eaa43afef6765b
Sebastian <sebgertz@gmail.com>
2019-01-08 21:46:55 -0500

    refactor: delete defaultProps, make center and zoom props inherited from MapContainer

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -3,18 +3,8 @@ import GoogleMapReact from 'google-map-react';
 import mapStyles from "../assets/mapStyles.js"
 
 class Map extends Component {
-
-  static defaultProps = {
-    center: {
-      lat: 40.77,
-      lng: -73.96
-    },
-    zoom: 14
-  };
-
   render() {
     const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY
-
     const mapOptions = {
       styles: mapStyles
     }
@@ -23,9 +13,9 @@ class Map extends Component {
       <div style={{ height: '100vh', width: '100%' }} id = "map">
         <GoogleMapReact
           bootstrapURLKeys={{ key: API_KEY }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
           options = {mapOptions}
+          center={this.props.center}
+          zoom={this.props.zoom}
         >
           {this.props.markers}
       </GoogleMapReact>
